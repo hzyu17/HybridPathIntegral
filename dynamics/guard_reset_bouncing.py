fixed the extension computation, debugging the feedback controlled trajectory on bouncing ball for jax ilqr

dynamics/guard_reset_bouncing.py
@@ -71,7 +71,7 @@ Rx_bouncing_12 = jax.jacrev(impact_map_bouncing)
 
 @jax.jit
 def reset_map_bouncing_12(x_minus, current_mode, args_reset):
-    x_plus = np.array([x_minus[0], -0.6*x_minus[1]], dtype=np.float64)
+    x_plus = jnp.array([x_minus[0], -0.6*x_minus[1]], dtype=jnp.float64)
     new_mode = 1
 
     return x_plus, new_mode, args_reset

dynamics/ode_solver/dynamics_bouncing.py
@@ -4,6 +4,7 @@ from matplotlib.font_manager import FontProperties
 font_props = FontProperties(family='serif', size=16, weight='normal')
 import sympy as sp
 
+
 def dyn_bouncing(t, x, *args):
     """
     Args:
@@ -18,6 +19,18 @@ def dyn_bouncing(t, x, *args):
         u = args[0]
     return np.array([x[1], u[0]-9.81], dtype=np.float64)
 
+
+def dyn_bouncing_jax(t, x, *args):
+      
+    if len(args) == 0:
+        u = jnp.array([0.0], dtype=jnp.float64)
+    else:
+        u = args[0]
+    return jnp.array([x[1], u[0]-9.81], dtype=jnp.float64)
+
+def f_euler_bouncing(x,u,dt):
+    return x + dyn_bouncing_jax(0.0, x, u)*dt
+    
 
 def gdWt_bouncing(x0, dWt, eps):
     B = np.array([[0],[1.0]], dtype=np.float64)
@@ -259,18 +272,14 @@ def event_detect_bouncing(x0, u, t0, tf, current_mode, reset_args, detection=Tru
     guards_bouncing_bouncing = {0:guard_bouncing_12, 1: guard_bouncing_21}
     reset_maps_bouncing_bouncing = {0:reset_map_bouncing_12, 1:reset_map_bouncing_21}
         
-    return event_detect_onestep(x0, 
-                                u, 
-                                t0, 
-                                tf, 
+    return event_detect_onestep(x0, u, 
+                                t0, tf, 
                                 current_mode, 
                                 smooth_dynamics_bouncing, 
                                 guards_bouncing_bouncing,
-                                gxs_bouncing,
-                                gts_bouncing,
+                                gxs_bouncing, gts_bouncing,
                                 reset_maps_bouncing_bouncing,
-                                Rxs_bouncing,
-                                Rts_bouncing,
+                                Rxs_bouncing, Rts_bouncing,
                                 reset_args, detection, backwards)
             
 
@@ -435,6 +444,10 @@ def plot_bouncingball(time_span, modes, states, inputs, init_state,
     ax2.set_xlabel(r"Time", fontproperties=font_props)
     ax2.set_ylabel(r"$\dot z$", fontproperties=font_props)
     ax2.set_title(r"Bouncing Ball Vertical Velocity", fontproperties=font_props)
+    
+    ax3.set_xlabel(r"$z$", fontproperties=font_props)
+    ax3.set_ylabel(r"$\dot z$", fontproperties=font_props)
+    ax3.set_title(r"Bouncing Ball State Plot", fontproperties=font_props)
 
     return fig1, axes_12, fig2, ax3
 

dynamics/trajectory_extension.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def compute_trejactory_extension(event_info, 
                                  timespan,
+                                 states,
                                  nx, nu, 
                                  init_state, target_state, 
                                  detection_func):
@@ -69,6 +70,16 @@ def compute_trejactory_extension(event_info,
         
         # Forward and backward trajectory extensions and (feedback, feedforward) gains for the two extensions
         for ii, tevent_i in enumerate(t_events[1:-1], start=1):
+            
+            if ii < len(t_events[1:-1]):
+                i_event_next = i_events[ii+1]
+            else:
+                i_event_next = len(timespan)
+            if ii > 1:
+                i_event_prev = i_events[ii-1]
+            else:
+                i_event_prev = 0
+            
             i_event_i = i_events[ii]
             x_event_i = x_events[ii]
             x_reset_i = x_resets[ii]
@@ -88,44 +99,35 @@ def compute_trejactory_extension(event_info,
             if len(dt) > 1:
                 dt_i = dt[i_event_i]
                 
-                # [0, t_event] for padding
-                time_span_ext_fwd_padding = timespan[:i_event_i]
-                # time_span_ext_fwd_padding = np.arange(0, tevent_i, dt_i)
+                # [0, t_event+1] for padding
+                time_span_ext_fwd_padding = timespan[:i_event_i+1]
+                time_span_ext_bwd_padding = timespan[i_event_i+1:]
                 
-                # # [t_event, t_trj_ext_fwd]
-                timespan_ext_fwd = timespan[i_event_i:]
-                # timespan_ext_fwd = np.arange(tevent_i, t_ext_fwd_i, dt_i)
-                
-                # # [t_trj_ext_bwd: t_event]
-                timespan_ext_bwd = timespan[:i_event_i][::-1]
-                # timespan_ext_bwd = np.arange(0, tevent_i, dt_i)[::-1]
+                timespan_ext_fwd = timespan[i_event_i+1:]
+                timespan_ext_bwd = timespan[:i_event_i+1][::-1]
                 
                 
-            else:
-                dt_i = dt
+            # else:
+            #     dt_i = dt
             
-                # [0, t_event] for padding
-                time_span_ext_fwd_padding = np.arange(0, tevent_i, dt_i)
+            #     # [0, t_event] for padding
+            #     time_span_ext_fwd_padding = np.arange(0, tevent_i, dt_i)
                 
-                # [t_event, t_trj_ext_fwd]
-                timespan_ext_fwd = np.arange(tevent_i, t_ext_fwd_i, dt_i)
+            #     # [t_event, t_trj_ext_fwd]
+            #     timespan_ext_fwd = np.arange(tevent_i, t_ext_fwd_i, dt_i)
                 
                 
-                # [t_trj_ext_bwd: t_event]
-                timespan_ext_bwd = np.arange(0, tevent_i, dt_i)[::-1]
+            #     # [t_trj_ext_bwd: t_event]
+            #     timespan_ext_bwd = np.arange(0, tevent_i, dt_i)[::-1]
             
             # time span lengths
             nt_ext_fwd = len(timespan_ext_fwd)
             nt_ext_bwd = len(timespan_ext_bwd)
-            nt_ext_padding_fwd = len(time_span_ext_fwd_padding)
-            nt_ext_padding_bwd = nt_ext_fwd
             
-            xtrj_ext_padding_fwd_i = np.zeros((nt_ext_padding_fwd, nx[mode_i]))
+            
             xtrj_ext_fwd_i = np.zeros((nt_ext_fwd, nx[mode_i]))
             xtrj_ext_fwd_i[0] = x_event_i
             
-            xtrj_ext_bwd_i = np.zeros((nt_ext_bwd, nx[next_mode_i]))
-            xtrj_ext_padding_bwd_i = np.zeros((nt_ext_padding_bwd, nx[next_mode_i]))    
             
             # Use the same gain for the whole extensions, and padding it to the whole time span
             K_feedback_ext_fwd_i = np.tile(K_feedback_fwd_extension_i, (nt, 1, 1))
@@ -152,7 +154,7 @@ def compute_trejactory_extension(event_info,
                 # Using zero control, modify if needed.
                 current_input = np.zeros(nu[mode_i])
                 
-                next_state, _, _, _, _, _, _ = detection_func(x_i, current_input, t_jj, t_jj+dt_jj, mode_i, detect=False, reset_args=None)
+                next_state, _, _, _, _, _, _ = detection_func(mode_i, x_i, current_input, t_jj, dt_jj, detect=False, reset_args=None)
                 
                 # Store states and inputs
                 xtrj_ext_fwd_i[jj+1] = next_state
@@ -160,11 +162,22 @@ def compute_trejactory_extension(event_info,
                 # Update the current state
                 x_i = next_state
             
+            # ------------------- Padding the forward extension -------------------
+            nt_ext_padding_fwd = len(time_span_ext_fwd_padding)
+            xtrj_ext_padding_fwd_i = np.zeros((nt_ext_padding_fwd, nx[mode_i]))
+            xtrj_ext_padding_fwd_i[i_event_prev+1:i_event_i+1] = np.asarray(states[i_event_prev+1:i_event_i+1])
+            
             xtrj_ext_fwd_i = np.vstack((xtrj_ext_padding_fwd_i, xtrj_ext_fwd_i))
             
             # ----------------------------------
             #   Simulate the backward extension
             # ----------------------------------
+            timespan_ext_bwd = timespan[:i_event_i+1][::-1]
+            # timespan_ext_bwd = np.arange(0, tevent_i, self.dt_)[::-1]
+            
+            # time span lengths
+            nt_ext_bwd = len(timespan_ext_bwd)
+            xtrj_ext_bwd_i = np.zeros((nt_ext_bwd, nx[next_mode_i]))
             xtrj_ext_bwd_i[0] = x_reset_i
             x_i = x_reset_i
             for jj in range(nt_ext_bwd-1):
@@ -177,7 +190,7 @@ def compute_trejactory_extension(event_info,
                 # modify if needed
                 current_input = np.zeros(nu[next_mode_i])
                 
-                next_state, _, _, _, _, _, _ = detection_func(x_i, current_input, t_jj, t_jj-dt_jj, next_mode_i, detect=False, reset_args=None)
+                next_state, _, _, _, _, _, _ = detection_func(next_mode_i, x_i, current_input, t_jj, -dt_jj, reset_args=None, detect=False)
                 
                 # Store states and inputs
                 xtrj_ext_bwd_i[jj+1] = next_state
@@ -190,7 +203,12 @@ def compute_trejactory_extension(event_info,
             # -------------------------------   
             xtrj_ext_bwd_i = xtrj_ext_bwd_i[::-1]
             
-            # --------------------- padding ---------------------
+            # --------------------- padding the backward extension ---------------------
+            
+            nt_ext_padding_bwd = len(time_span_ext_bwd_padding)
+            xtrj_ext_padding_bwd_i = np.zeros((nt_ext_padding_bwd, nx[next_mode_i]))    
+            xtrj_ext_padding_bwd_i[:i_event_next-i_event_i] = np.asarray(states[i_event_i+1:i_event_next+1])
+            
             xtrj_ext_bwd_i = np.vstack((xtrj_ext_bwd_i, xtrj_ext_padding_bwd_i))
             
             # ------------------------ collect the trajectory extensions ------------------------

hybrid_ilqr/h_ilqr_discrete.py
@@ -184,6 +184,18 @@ class hybrid_ilqr:
             A_trj[idx] = A_k
             B_trj[idx] = B_k
             
+            
+            # print("self._states[-1]: ", self._states[-1])
+            # print("self._target_state: ", self._target_state)
+            # print("V_x: ", V_x)
+            # print("V_xx: ", V_xx)
+            # print("l_x: ", l_x)
+            # print("l_u: ", l_u)
+            # print("l_xx: ", l_xx)
+            # print("l_uu: ", l_uu)
+            # print("A_k: ", A_k)
+            # print("B_k: ", B_k)
+            
             if saltation is None:
             
                 Q_x = l_x*self.dt_ + A_k.T@V_x
@@ -196,7 +208,16 @@ class hybrid_ilqr:
                 k = -np.linalg.solve(Q_uu, Q_u)
                 K = -np.linalg.solve(Q_uu, Q_ux).reshape((self._n_inputs[current_mode], self._n_states[current_mode]))
 
-            else:                
+                # print("Q_x: ", Q_x)
+                # print("Q_u: ", Q_u)
+                # print("Q_ux: ", Q_ux)
+                # print("Q_uu: ", Q_uu)
+                # print("Q_xx: ", Q_xx)
+                # print("k: ", k)
+                # print("K: ", K)
+                
+            else:
+                # print("saltation_i: ", saltation)                
                 # print("Found contact dynamics! Computing the gains with saltation matrix.")
                 Q_x = l_x*self.dt_ + A_k.T @ saltation.T @ V_x
                 Q_u = l_u*self.dt_ + B_k.T @ saltation.T @ V_x
@@ -288,19 +309,23 @@ class hybrid_ilqr:
              v_Kfb_ext_trj_bwd_ref, v_Kfb_ext_trj_fwd_ref, 
              v_kff_ext_trj_bwd_ref, v_kff_ext_trj_fwd_ref, v_tevents_ref) = extract_extensions(self._reference_extension_helper)
 
-            # # ------------------------ Plot reference extensions ------------------------ 
-            # states_arr = np.asarray(self._states)
-            # fig, ax = plt.subplots()
-            # ax.grid(True)
-            # ax.plot(states_arr[:,0], states_arr[:,1], 'k')
-            # for i_ext_trj_bwd in v_ext_trj_bwd_ref:
-            #     i_ext_trj_bwd = v_ext_trj_bwd_ref[2]
-            #     ax.plot(i_ext_trj_bwd[:,0], i_ext_trj_bwd[:,1], 'r')
-            # for i_ext_trj_fwd in v_ext_trj_fwd_ref: 
-            #     i_ext_trj_fwd =  v_ext_trj_fwd_ref[2]
-            #     ax.plot(i_ext_trj_fwd[:,0], i_ext_trj_fwd[:,1], 'b')
+            # ------------------------ Plot reference extensions ------------------------ 
+            states_arr = np.asarray(self._states)
+            fig, ax = plt.subplots()
+            ax.grid(True)
+            ax.plot(states_arr[:,0], states_arr[:,1], 'k')
+            for i_ext_trj_bwd in v_ext_trj_bwd_ref[1:2]:
+                ax.plot(i_ext_trj_bwd[:,0], i_ext_trj_bwd[:,1], 'r', label="backward extension")
+            for i_ext_trj_fwd in v_ext_trj_fwd_ref[1:2]: 
+                ax.plot(i_ext_trj_fwd[:,0], i_ext_trj_fwd[:,1], 'b', label="forward extension")
+                
+            ax.legend()
+            ax.set_xlabel(r"$z$")
+            ax.set_ylabel(r"$\dot z$")
+            ax.set_title(r"Bouncing Ball State Plot")
 
-            # plt.show()
+            plt.show()
+            
             if self._verbose:
                 print(f"Reference trajectory bouncing event numbers: {len(v_ext_trj_bwd_ref)}")
                 for i_bounce in range(len(v_ext_trj_bwd_ref)):
@@ -484,7 +509,10 @@ class hybrid_ilqr:
             # --------------------------------------------------------
             # Compute the backwards pass and update the control gains
             # --------------------------------------------------------
-            (k_feedforward,K_feedback,expected_reduction,A_trj,B_trj) = self.backward_pass()    
+            (k_feedforward,
+             K_feedback,
+             expected_reduction,
+             A_trj,B_trj) = self.backward_pass()    
             
             # --------------------------------------------------------------
             #  Compute the new trajectory extensions and the gains for them
@@ -514,7 +542,7 @@ class hybrid_ilqr:
                  new_hybrid_event_info,new_reset_args)=self.forward_pass(learning_rate)
                 
                 # --------------------------------- Plot forward pass ---------------------------------
-                show_fwdpass = False
+                show_fwdpass = True
                 if show_fwdpass:
                     self._plot_states_func(self._timespan, modes, states, inputs, 
                                             self._init_state, self._target_state, 
@@ -697,8 +725,7 @@ class hybrid_ilqr:
                 
                 next_state, _, _, _, _, _, _ = self.detection_func_(current_mode_i, current_state, 
                                                                     current_input, t_jj, dt, 
-                                                                    # self._dtshrinkrate, 
-                                                                    self._reset_args[jj], detection=False)
+                                                                    self._reset_args[jj], detect=False)
              
                 # Store states and inputs
                 xtrj_ext_fwd_i[jj+1] = next_state
@@ -741,9 +768,8 @@ class hybrid_ilqr:
                 
                 next_state, _, _, _, _, _, _ = self.detection_func_(next_mode_i,current_state, 
                                                                     current_input, t_jj, dt, 
-                                                                    # self._dtshrinkrate, 
                                                                     self._reset_args[jj], 
-                                                                    detection=False, 
+                                                                    detect=False, 
                                                                     backwards=True)
                 
                 # Store states and inputs